b09fae190ef9f1490ec9dfa42b1319f201648a85
kamrulislam2 <kamrulislamABK@gmail.com>
2023-05-21 01:03:29 +0600

    spinner for private route added

--- src/Routers/PrivateRoute.jsx
+++ src/Routers/PrivateRoute.jsx
@@ -2,13 +2,24 @@ import React, { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 import Swal from "sweetalert2";
+import PuffLoader from "react-spinners/ClipLoader";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
   if (loading) {
-    return <div>Loading</div>;
+    return (
+      <div className="flex items-center justify-center p-40">
+        <PuffLoader
+          color="#F79837"
+          cssOverride={{}}
+          loading
+          size={70}
+          speedMultiplier={1}
+        />
+      </div>
+    );
   }
   if (user) {
     return children;
